657ff399e5447497b0eed6edf55ce498aaa55634
Vaibhav Shrivastava <vaibhavmishu@gmail.com>
2018-08-16 23:59:16 +0530

    feat: Confirm dialog on exiting Update Organizer Info

--- app/src/main/java/com/eventyay/organizer/core/organizer/detail/OrganizerDetailActivity.java
+++ app/src/main/java/com/eventyay/organizer/core/organizer/detail/OrganizerDetailActivity.java
@@ -3,7 +3,9 @@ package com.eventyay.organizer.core.organizer.detail;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ContextThemeWrapper;
 import android.view.MenuItem;
 
 import com.eventyay.organizer.R;
@@ -17,6 +19,7 @@ import dagger.android.support.HasSupportFragmentInjector;
 public class OrganizerDetailActivity extends AppCompatActivity implements HasSupportFragmentInjector {
 
     private final FragmentManager fragmentManager = getSupportFragmentManager();
+    private AlertDialog saveAlertDialog;
 
     @Inject
     DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
@@ -50,5 +53,24 @@ public class OrganizerDetailActivity extends AppCompatActivity implements HasSup
     @Override
     public AndroidInjector<Fragment> supportFragmentInjector() {
         return dispatchingAndroidInjector;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (fragmentManager.getBackStackEntryCount() == 2 && saveAlertDialog == null && id != -1) {
+            saveAlertDialog = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.AlertDialog))
+                .setMessage(getString(R.string.save_changes))
+                .setPositiveButton(getString(R.string.stay), (dialog, which) -> {
+                    //do nothing
+                })
+                .setNegativeButton(getString(R.string.discard), (dialog, which) -> {
+                    dialog.dismiss();
+                    super.onBackPressed();
+                })
+                .create();
+            saveAlertDialog.show();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
